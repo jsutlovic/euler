542c8faf7773a45ff4203eccce75c0b5451b692f
Jero Sutlovic <jsutlovic@gmail.com>
2012-10-31 09:31:41 -0400

    Finished Problem 2

--- problem2.go
+++ problem2.go
@@ -7,18 +7,29 @@ package main
  *  exceed four million, find the sum of the even-valued terms.
  */
 
-const N = 100
+const N = 4000000
 
 func main() {
+	fib := fibonacci()
+	i := fib()
+	sum := 0
 
+	for i < N {
+		if i%2 == 0 {
+			sum += i
+		}
+		i = fib()
+	}
+
+	println(sum)
 }
 
-func fibonacci() func(c chan <- int) {
-    var f1, f2 = 0, 1
-    f := func(c chan <- int) {
-        f0 := f1
-        f1, f2 = f2, f1+f2
-        c <- f0
-    }
-    return f
+func fibonacci() func() int {
+	var f1, f2 = 0, 1
+	f := func() int {
+		f0 := f1
+		f1, f2 = f2, f1+f2
+		return f0
+	}
+	return f
 }
